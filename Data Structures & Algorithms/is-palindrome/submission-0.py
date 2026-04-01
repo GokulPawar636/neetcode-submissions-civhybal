a7a794b8944a11c19623055d6d4b89e984234406
Gokul Atmaram Pawar <gokulpawar636@gmail.com>
2026-04-02 00:04:20 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean =""
+        for ch in s:
+            if ch.isalnum():
+                clean+=ch.lower()
+
+        n = len(s)
+        left = 0
+        right = len(clean)-1
+
+        while left <=right:
+            if clean[left]!=clean[right]:
+                return False
+            left+=1
+            right-=1
+        return True
